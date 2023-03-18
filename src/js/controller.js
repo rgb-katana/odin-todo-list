3f0d1af27f132bc9eacb0f70a2bbc15c0d1180a0
KirMac <komarova1610@yandex.ru>
2023-03-18 22:47:27 +0300

    fix the plus button on todo list

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,43 +1,47 @@
-'use strict';
+"use strict";
 
-import '../sass/main.scss';
-import * as model from './model';
-import * as todosView from './views/todosView';
-import * as projectsView from './views/projectsView';
+import "../sass/main.scss";
+import * as model from "./model";
+import * as todosView from "./views/todosView";
+import * as projectsView from "./views/projectsView";
 
 function controlAddProject(newProject) {}
 
 function controlAddTodo(newTodo) {}
 
 function selectProject() {
-  document.addEventListener('click', function (e) {
-    if (e.target.closest('.project')) {
-      // Delete all selections
-      Array.from(document.querySelectorAll('.project')).map(elem => {
-        elem.classList.remove('chosen');
-      });
+    document.addEventListener("click", function (e) {
+        if (e.target.closest(".project")) {
+            // Delete all selections
+            Array.from(document.querySelectorAll(".project")).map((elem) => {
+                elem.classList.remove("chosen");
+            });
 
-      e.target.closest('.project').classList.add('chosen');
-      const projectNum = e.target.closest('.project').dataset.projectNum;
-      todosView.renderTodos(
-        model.state.projects[`pr${projectNum}`].title,
-        model.state.projects[`pr${projectNum}`].todos
-      );
-    }
-  });
+            e.target.closest(".project").classList.add("chosen");
+            const projectNum = e.target.closest(".project").dataset.projectNum;
+            todosView.renderTodos(
+                model.state.projects[`pr${projectNum}`].title,
+                model.state.projects[`pr${projectNum}`].todos
+            );
+            todosView.RenderPlusTodo();
+        }
+    });
 }
 
 function createNewProject() {
-  document.addEventListener('click', function (e) {
-    console.log(e.target.closest('.projects__add-btn'));
-  });
+    document.addEventListener("click", function (e) {
+        console.log(e.target.closest(".projects__add-btn"));
+    });
 }
 
 function init() {
-  projectsView.renderProjects(model.state.projects, model.state.projectCounter);
-  projectsView.renderPlusProject();
-  selectProject();
-  createNewProject();
+    projectsView.renderProjects(
+        model.state.projects,
+        model.state.projectCounter
+    );
+    projectsView.renderPlusProject();
+    selectProject();
+    createNewProject();
 }
 
 init();
--- src/js/views/todosView.js
+++ src/js/views/todosView.js
@@ -1,71 +1,72 @@
-'use strict';
+"use strict";
 
-import { createEl } from '../helpers';
-import expandSvg from '../../img/arrow-expand.svg';
-import deleteSvg from '../../img/delete.svg';
+import { createEl } from "../helpers";
+import expandSvg from "../../img/arrow-expand.svg";
+import deleteSvg from "../../img/delete.svg";
+import plusSvg from "../../img/plus.svg";
 
-const projectTitle = document.querySelector('.todos__title');
-const todosLists = document.querySelector('.todos__list');
-const addTodoBtn = document.querySelector('.todos__add');
+const projectTitle = document.querySelector(".todos__title");
+const todosLists = document.querySelector(".todos__list");
+const addTodoBtn = document.querySelector(".todos__add");
 
 export function renderTodos(title, todos) {
-  projectTitle.innerText = title;
-  todosLists.innerHTML = '';
+    projectTitle.innerText = title;
+    todosLists.innerHTML = "";
 
-  for (const todo in todos) {
-    const curTodo = todos[todo];
+    for (const todo in todos) {
+        const curTodo = todos[todo];
 
-    const todoEl = createEl('div', ['todos__todo', 'todo']);
+        const todoEl = createEl("div", ["todos__todo", "todo"]);
 
-    const todoTitle = createEl('h3', ['todo__title'], curTodo['title']);
-    const todoDueDate = createEl('h4', ['todo__due-date'], 'Due: ');
-    todoDueDate.insertAdjacentHTML(
-      'beforeend',
+        const todoTitle = createEl("h3", ["todo__title"], curTodo["title"]);
+        const todoDueDate = createEl("h4", ["todo__due-date"], "Due: ");
+        todoDueDate.insertAdjacentHTML(
+            "beforeend",
+            `
+      <span class="date">${curTodo["due"]}</span>
       `
-      <span class="date">${curTodo['due']}</span>
-      `
-    );
-    const todoControls = createEl('div', ['todo__controls']);
-    const expandBtn = createEl('button', ['todo__expand']);
-    expandBtn.insertAdjacentHTML(
-      'afterbegin',
-      `
+        );
+        const todoControls = createEl("div", ["todo__controls"]);
+        const expandBtn = createEl("button", ["todo__expand"]);
+        expandBtn.insertAdjacentHTML(
+            "afterbegin",
+            `
       <svg>
         <use href="${expandSvg}#expand"></use>
       </svg>
       `
-    );
-    const deleteBtn = createEl('button', ['todo__delete']);
-    deleteBtn.insertAdjacentHTML(
-      'afterbegin',
-      `
+        );
+        const deleteBtn = createEl("button", ["todo__delete"]);
+        deleteBtn.insertAdjacentHTML(
+            "afterbegin",
+            `
     <svg>
       <use href="${deleteSvg}#delete"></use>
     </svg>
     `
-    );
+        );
 
-    todoControls.append(expandBtn, deleteBtn);
-    todoEl.append(todoTitle, todoDueDate, todoControls);
-    todosLists.append(todoEl);
-  }
+        todoControls.append(expandBtn, deleteBtn);
+        todoEl.append(todoTitle, todoDueDate, todoControls);
+        todosLists.append(todoEl);
+    }
 }
 
 function createAddTodo() {
-  const addContainer = createEl('div', ['todos__add']);
-  const addBtn = createEl('button', ['todoss__add-btn']);
-  addBtn.insertAdjacentHTML(
-    'afterbegin',
-    `
+    const addContainer = createEl("div", ["todos__add"]);
+    const addBtn = createEl("button", ["todos__add-btn"]);
+    addBtn.insertAdjacentHTML(
+        "afterbegin",
+        `
     <svg>
       <use href="${plusSvg}#plus"></use>
     </svg>
     `
-  );
-  addContainer.append(addBtn);
-  return addContainer;
+    );
+    addContainer.append(addBtn);
+    return addContainer;
 }
 
 export function RenderPlusTodo() {
-  todosLists.append(createAddTodo());
+    todosLists.append(createAddTodo());
 }
